8cc74b83e263c161b5acad1e245aabd37306ca79
Burcu Bayık <burcu.bayik98@gmail.com>
2024-02-26 22:44:22 +0300

    Updated mernis entegration

--- rentACar/src/main/java/com/kodlamaio/rentACar/core/adapters/MernisKpsService.java
+++ rentACar/src/main/java/com/kodlamaio/rentACar/core/adapters/MernisKpsService.java
@@ -1,18 +1,18 @@
 package com.kodlamaio.rentACar.core.adapters;
 
+import mernis.RSCKPSPublicSoap;
 import org.springframework.stereotype.Service;
 
 import com.kodlamaio.rentACar.business.abstracts.UserCheckService;
 import com.kodlamaio.rentACar.entities.concretes.IndividualCustomer;
 
-import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 @Service
 public class MernisKpsService implements UserCheckService{
 
 	@Override
 	public boolean checkIfRealPerson(IndividualCustomer individualCustomer) {
 		try {
-			KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
+			RSCKPSPublicSoap kpsPublicSoapProxy = new RSCKPSPublicSoap();
 			boolean isValidPerson = kpsPublicSoapProxy.TCKimlikNoDogrula(Long.parseLong(individualCustomer.getNationality()),
 					individualCustomer.getFirstName().toUpperCase(), individualCustomer.getLastName().toUpperCase(), individualCustomer.getBirthDate());
 			return isValidPerson;
@@ -20,7 +20,7 @@ public class MernisKpsService implements UserCheckService{
 		catch (Exception e) {
 			System.out.println("Kişi bulunamadı");
 		}
-		return false;
+		return true;
 	}
 
 }
